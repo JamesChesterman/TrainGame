62e7f6c1e6f6738cbb1700055fbe8bf6ceaef7e6
JamesChesterman <chesterman261@sky.com>
2023-06-15 15:34:02 +0100

    Tracks placed below another track become part of the same route

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -11,8 +12,11 @@ public class GameController : MonoBehaviour
     private Transform currentPoint;
     public Vector3 differencePerColumn = new Vector3(3.2f, 0f, 0f);
     public Vector3 differencePerRow = new Vector3(0f, 0f, -3.2f);
-    
+
     private int[,] tracks;
+    //This will be an arraylist of arraylists (routes)
+    //Each subarraylist (route) will consist of arrays of length 2 (coordinates)
+    private List<List<int[]>> routeList2D = new List<List<int[]>>();
 
     private void initialiseArrays(){
         GameObject[,] levelBlockArray = new GameObject[levelHeight, levelWidth];
@@ -36,10 +40,54 @@ public class GameController : MonoBehaviour
         }
     }
 
+    //private void checkIfTrackStartOrEndOfRoute(){
+
+    //}
+
+    private void checkIfNewRoute(int i, int j){
+        //If it's next to a track (not including diagonals) AND this track is the end of a route 
+        //Then the newly placed track becomes part of the same route
+        //Look up
+        //Need to check that i isn't zero
+        if(tracks[i-1, j] == 1){
+            for(int p=0; p<routeList2D.Count; p++){
+                //Check each route to see if these coordinates are at the start or end of each route
+                int[] startOfRoute = routeList2D[p][0];
+                int[] endOfRoute = routeList2D[p][routeList2D[p].Count-1];
+                if(startOfRoute[0] == (i-1) && startOfRoute[1] == j){
+                    //Add new track to start of route
+                    routeList2D[p].Insert(0, new int[] {i,j});
+                    //Need to check that there aren't any other tracks that the track is also connected to
+                    //So if the new track is between two pieces of track, it needs to join the 2 together
+
+                    return;
+                }else if(endOfRoute[0] == (i-1) && endOfRoute[1] == j){
+                    //Add new track to end of route
+                    routeList2D[p].Add(new int[] {i, j});
+                    //Need to check that there aren't any other tracks that the track is also connected to
+                    //So if the new track is between two pieces of track, it needs to join the 2 together
+
+                    return;
+                }
+            }
+        }
+
+        //Checked all directions, there's nothing surrounding the new track that is a start / end point of a route
+        //Make new route
+        routeList2D.Add(new List<int[]> ());
+        routeList2D[routeList2D.Count-1].Add(new int[] {i,j});
+    }
+
     public void setTrackPlaced(int i, int j){
         tracks[i,j] = 1;
         //Print the array
         printArray(tracks);
+        checkIfNewRoute(i,j);
+        string dynamicArrayAsString = string.Join(", ",
+            routeList2D.Select(subarray =>
+                $"[{string.Join(", ", subarray.Select(arr => $"[{string.Join(", ", arr)}]"))}]"
+            ));
+        Debug.Log(dynamicArrayAsString);
     }
 
     public void printArray(int[,] array2D){
